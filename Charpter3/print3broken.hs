29567ef4a6912c78de33d22f4822ce93e04fe840
王增迪 <wangzengdi@meituan.com>
2017-12-26 08:04:41 +0800

    Strings: Simple Operations with Text

---
+++ Charpter3/print3broken.hs
@@ -0,0 +1,10 @@
+module Print3Broken where
+
+printSecond :: IO ()
+printSecond = do putStrLn greeting
+
+main :: IO ()
+main = do
+  putStrLn greeting
+  printSecond
+  where greeting = "Yarrrrrr"
---
+++ Charpter3/thirdLetter.hs
@@ -0,0 +1,8 @@
+thirdLetter :: String -> Char
+thirdLetter x = x !! 3
+
+letterIndex :: Int -> Char
+letterIndex x = "Curry is awesome!" !! x
+
+rvrs :: String -> String
+rvrs x = (drop 9 x) ++ " " ++ (take 2 $ drop 6 x) ++ " " ++ (take 5 x)
